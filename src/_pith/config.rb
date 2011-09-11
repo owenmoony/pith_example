8664cd7958af9745b6b5cf464433bbca4b3508de
Owen Moony <owenm@boxdice.com.au>
2011-09-11 10:49:44 +1000

    Added stuff to .gitignore

--- src/_pith/config.rb
+++ src/_pith/config.rb
@@ -1,10 +1,10 @@
 project.helpers do
 
-  def navigation_items(current)
+  def navigation_items
     items = ["one", "two", "three"].collect do |item|
       %{ <li><a href="#{item}">#{item}</a> }
     end
-    items.join("<li>\n")
+    items.join("\n")
   end
 
 end
